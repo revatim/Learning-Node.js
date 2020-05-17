26bc4781f1f2f1b539a082b6dd21408a82fcc48a
Revati Mahajan <mahajan21revati@gmail.com>
2020-05-17 21:48:03 +0530

    section9

--- Section 4 - Improved Development Workflow and Debugging/app.js
+++
@@ -1,8 +0,0 @@
-// require - importing in node.js -- ./ local file (adds js automatically)
-const http = require('http');
-
-const routes = require('./routes');
-
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
